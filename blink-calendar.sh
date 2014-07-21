a4ecba72f6b17956c1306af2c3235046a528a542
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-07-21 14:38:51 +0100

    limit number of attempts

--- blink-calendar.sh
+++ blink-calendar.sh
@@ -5,10 +5,13 @@ do
 	date=`date`
 	echo "$date   checking..."
 	result=`icalBuddy -iep title -ic Calendar -ea 'eventsNow' | wc -l`
+
+	attempts=1
 	if (( result > 0 )) 
 	then
-		until /usr/local/bin/blink1-tool -m 200 -t 400 --yellow --blink 3
+		until /usr/local/bin/blink1-tool -m 200 -t 400 --yellow --blink 3 || [ $attempts -gt 5 ]
 		do
+			attempts=$((attempts + 1))
 			sleep 2
 		done
 	fi
--- blink-gmail.sh
+++ blink-gmail.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
+attempts=1
 result=`/Users/abraithwaite/scripts/gmail.pl`
 if (( result > 0 )) 
 then
-	until /usr/local/bin/blink1-tool -m 200 -t 400 --blue --blink 3
+	until /usr/local/bin/blink1-tool -m 200 -t 400 --blue --blink 3 || [ $attempts -gt 5 ]
 	do
+		attempts=$((attempts + 1))
 		sleep 2
 	done
 fi
