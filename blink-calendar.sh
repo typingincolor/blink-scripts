f3e8a298d15cc850b29c0c0ba2c70a260ea5ae54
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-07-21 14:27:53 +0100

    wait until succeeds

--- blink-calendar.sh
+++ blink-calendar.sh
@@ -7,8 +7,11 @@ do
 	result=`icalBuddy -iep title -ic Calendar -ea 'eventsNow' | wc -l`
 	if (( result > 0 )) 
 	then
-		/usr/local/bin/blink1-tool -m 200 -t 400 --yellow --blink 3
+		until /usr/local/bin/blink1-tool -m 200 -t 400 --yellow --blink 3
+		do
+			sleep 2
+		done
 	fi
 
-	sleep 300 
+	sleep 180 
 done
--- blink-gmail.sh
+++ blink-gmail.sh
@@ -4,5 +4,8 @@
 result=`/Users/abraithwaite/scripts/gmail.pl`
 if (( result > 0 )) 
 then
-	/usr/local/bin/blink1-tool -m 200 -t 400 --blue --blink 3
+	until /usr/local/bin/blink1-tool -m 200 -t 400 --blue --blink 3
+	do
+		sleep 2
+	done
 fi
